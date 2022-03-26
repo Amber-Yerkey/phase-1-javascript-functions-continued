79ea79c313afce4e001b3ffa66166f9316f0ee9d
Amber Frantz <amber.k.frantz@mgail.com>
2022-03-26 09:40:35 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+
+function saturdayFun(event = "roller-skate"){
+    return `This Saturday, I want to ${event}!`
+}
+
+function mondayWork(event = "go to the office"){
+    return `This Monday, I will ${event}.`
+}
+
+function wrapAdjective(flair = "*") {
+    function inner(word = "special") {
+      return `You are ${flair}${word}${flair}!`
+    }
+    return inner
+  }
